35ad79898ee93cfde5d942aedd893308441c8a85
Martin R. Albrecht <martinralbrecht@googlemail.com>
2020-09-11 10:39:18 +0100

    migrated

--- code/.dir-locals.el
+++ code/.dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((nil . ((project-venv-name . "project-name")))
+((nil . ((conda-project-env-path . "project-name")))
  (python-mode . ((show-trailing-whitespace t)
                  (blacken-line-length . 120)
                  (eval . (blacken-mode)))))
