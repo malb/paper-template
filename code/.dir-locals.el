61678a2afe8bd263ae9da9fa78cf2ae5fd3633bf
Martin R. Albrecht <martinralbrecht@googlemail.com>
2025-10-25 15:29:37 +0100

    nope

--- code/.dir-locals.el
+++ code/.dir-locals.el
@@ -1,7 +1,6 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((nil . ((conda-project-env-path . "project-name")))
- (python-mode . ((show-trailing-whitespace t)
+((python-mode . ((show-trailing-whitespace t)
                  (blacken-line-length . 120)
                  (eval . (blacken-mode)))))
